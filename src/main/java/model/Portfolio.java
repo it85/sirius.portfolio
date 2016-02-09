c59d818a5df84de2a3a3e543b547d15c7c17ee74
Isaac Tyan <isaac.tyan@gmail.com>
2016-02-08 20:27:33 -0500

    created Portfolio class

---
+++ src/main/java/model/Portfolio.java
@@ -0,0 +1,15 @@
+package model;
+
+import contract.IPortfolio;
+
+public class Portfolio implements IPortfolio{
+
+	public void buy(String cusip, int shares, double price) {
+		
+	}
+
+	public void sell(String cusip, int shares, double price) {
+		
+	}
+
+}
