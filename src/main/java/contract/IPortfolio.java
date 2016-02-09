9999b7f0a315abd7371b53b097c9a283bf72c26b
Isaac Tyan <isaac.tyan@gmail.com>
2016-02-08 20:26:24 -0500

    added documentation for IPortfolio

--- src/main/java/contract/IPortfolio.java
+++ src/main/java/contract/IPortfolio.java
@@ -1,5 +1,10 @@
 package contract;
 
+/**
+ * Defines a set of behaviors for a Portfolio instance. This is the interface that is exposed to the world to use.
+ * @author Isaac T
+ *
+ */
 public interface IPortfolio {
 	
 	void buy(String cusip, int shares, double price);
